52a1b5265dafb36001855b305b4aab189800e559
AATIQ AFZAL <77941370+aatiq1443@users.noreply.github.com>
2024-10-08 21:56:12 +0530

    Add files via upload

---
+++ frontend-boilerplate-react-tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-500'>
+        hi tubbu betu
+        I've checked the tailwind property you can proceed your work from here!!!!!!!! 
+      </div>
+    </>
+  )
+}
+
+export default App
